b85ce71d95d436280b6b51350bee45246a19225c
Anto0524 <139234061+Anto0524@users.noreply.github.com>
2023-07-12 21:30:23 +0530

    task1_antojerin_ifelse

---
+++ task1_antojerin_ifelse.py
@@ -0,0 +1,11 @@
+#hotstar membership
+a=int(input('enter the number'))
+if a>=1 and a<=5:
+    print(a*(5),'fine')
+elif a>=6 and a<=10:
+    print(a*10,'fine')
+elif a>=11 and a<=30:
+    print(a*15,'fine')
+else:
+    print('membership cancelled')
+  
