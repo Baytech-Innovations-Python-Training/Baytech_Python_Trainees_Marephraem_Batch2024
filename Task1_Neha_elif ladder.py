55d0cc793c2695139281f3f052bf312e3f36aca0
DAneha-2002 <139232702+DAneha-2002@users.noreply.github.com>
2023-07-12 20:39:44 +0530

    Task1_Neha

---
+++ Task1_Neha_elif ladder.py
@@ -0,0 +1,15 @@
+#prime hotstar membership
+#elif ladder
+days=int(input("Enter the number of days:"))
+if days<=0:
+    print("Good No Fine")
+elif days>=1 and days<=5:
+    print("Fine Amount:5")
+elif days>=5 and days<=10:
+    print("Fine Amount:10")
+elif days>=10 and days<=30:
+    print("Fine Amount:15")
+else:
+    print("Membership Cancel")
+
+
