c554d410717c22f4af547f081c3ca4afb37897e2
merinshajeffy <132051743+merinshajeffy@users.noreply.github.com>
2023-07-13 08:54:46 +0530

    Task1_Merinsha jeffy

---
+++ Task 1_Merinsha jeffy_elif.py
@@ -0,0 +1,26 @@
+#calculate some add sub div
+a=int(input("enter the value:"))
+b=int(input("enter the value :"))
+c=int(input("enter the number:"))
+if c==1:
+      add=a+b
+      print(add)
+elif c==2:
+      sub=a-b
+      print(sub)
+elif c==3:
+      multi=a*b
+      print(multi)
+elif c==4:
+      divi=a/b
+      print(divi)
+elif c==5:
+      modolo=a%b
+      print(modolo)
+else :
+      print(invalid)
+
+      
+ 
+ 
+      
