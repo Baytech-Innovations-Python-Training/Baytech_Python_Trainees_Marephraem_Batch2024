428f698c6279c999107c9bd1504ed73315156fcb
Raghul4321 <139291504+Raghul4321@users.noreply.github.com>
2023-07-13 12:00:01 +0530

    Task1_Raghul

---
+++ Task1_Raghul main.py
@@ -0,0 +1,18 @@
+
+
+a=int(input("Enter the value"))
+b=int(input("Enter the value"))
+n=int(input("Enter the value"))
+if n==1:
+    print("To add two number ",a+b)
+elif n==2:
+    print("To substract two number",a-b)
+elif n==3:
+    print("To multiply two nos",a*b)
+elif n==4:
+    print("TO Divide two nos",a/b)
+elif n==5:
+    print("To modulo",a%b)
+else:
+    print("invalid")
+
