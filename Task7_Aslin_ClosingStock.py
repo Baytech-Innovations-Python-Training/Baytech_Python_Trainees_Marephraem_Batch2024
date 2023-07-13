b53e4a95cd2ba0b1286965c957858ea8a2dc7e0c
AslinAmmu <113002668+AslinAmmu@users.noreply.github.com>
2023-07-13 20:25:09 +0530

    Task7_Aslin

---
+++ Task7_Aslin_ClosingStock.py
@@ -0,0 +1,7 @@
+ #closing stock
+
+openingstock=int(input("enter the openingstock:"))
+purchase=int(input("enter the purchase amount:"))
+sales=int(input("enter the sales:"))
+closingstock=(openingstock+purchase-sales)
+print(closingstock)
