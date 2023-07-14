7fbe4fe484228938f47c2a2764061b33785a3e60
DibenaDavid <132053560+DibenaDavid@users.noreply.github.com>
2023-07-14 08:50:17 +0530

    Task7_Dibena David

---
+++ task7.dibenadavid_closing_stock.py
@@ -0,0 +1,5 @@
+openintstock=os=int(input("enter the  openintstock="))
+purchase=pr=int(input("enter the  purchase="))
+sales=s=int(input("enter the sales="))
+closed_stock=os+pr-s
+print(closed_stock)
