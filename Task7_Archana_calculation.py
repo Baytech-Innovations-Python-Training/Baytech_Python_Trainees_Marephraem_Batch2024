70337258cf3f850dc11b8e0ec25e4bf441d56c9b
11RAArchana <132084812+11RAArchana@users.noreply.github.com>
2023-07-14 06:48:35 +0530

    Task7_Archana

---
+++ Task7_Archana_calculation.py
@@ -0,0 +1,13 @@
+
+#calculate
+openingstack=int(input("Enter the openingstack:"))
+purchase=int(input("purchase:"))
+sales=int(input("sales:"))
+closingstock=openingstack+purchase-sales
+print("closingstock:",closingstock)
+
+
+
+
+
+
