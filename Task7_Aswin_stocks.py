cc89a059defafd7943240d2f5f6f253746c845fa
Aswin-t-r <139233615+Aswin-t-r@users.noreply.github.com>
2023-07-14 13:53:07 +0530

    Task7_Aswin

---
+++ Task7_Aswin_stocks.py
@@ -0,0 +1,11 @@
+#stocks
+#getting opening stock as input 
+os=int(input("Opening Stock :"))
+#getting purchase as input 
+p=int(input("Purchase :"))
+#getting sales as input 
+s=int(input("Sales :"))
+#calculating the closing stock
+cs=os+p-s
+#printimg the closing stock
+print ("Closing Stock :",cs)
