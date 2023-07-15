4c3dbc9e0367fab45b6506db31d5d5284b1f58cf
41-S. Neeshma <117502615+neeshmasneeshmas@users.noreply.github.com>
2023-07-15 10:30:33 +0530

    Task7_Neeshma

---
+++ Task7_Neeshma_Open Stacking.py
@@ -0,0 +1,7 @@
+#Calculating a OpenStacking
+Open_Stacking=int(input("Enter the Open_Stacking:"))
+Purchase=int(input("Enter the Purchase:"))
+Sales=int(input("Enter the Sales:"))
+Closing_Stack=Open_Stacking+Purchase-Sales
+print("The Closing_Stack is ",Closing_Stack)
+
