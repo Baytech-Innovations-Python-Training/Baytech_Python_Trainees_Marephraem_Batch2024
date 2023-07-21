bf28635b3b36a5ffc45fb893cd43057108396f4b
Anto0524 <139234061+Anto0524@users.noreply.github.com>
2023-07-22 00:32:59 +0530

    task7_antojerin

--- task7_antojerin_for and while.py
+++ task7_antojerin_for and while.py
@@ -1,5 +1,20 @@
-x=int(input('enter the number'))
-for i in range(x,0,-1):
-    print(i,end='')
-
-
+#descending order using while loop
+num=int(input("enter the number of limit"))
+x=num
+while x>=1:
+    print(x,end=" ")
+    x-=1
+#ascending order using while loop
+num=int(input("enter the number of limit"))
+x=1
+while x<=num:
+    print(x,end=" ")
+    x+=1
+#descending order using for loop
+num=int(input("enter the number of limit"))
+for i in range(num,0,-1):
+    print(i,end=" ")
+#ascending order using for loop
+num=int(input("enter the number of list"))
+for i in range(1,num,+1):
+    print(i,end=" ")  
